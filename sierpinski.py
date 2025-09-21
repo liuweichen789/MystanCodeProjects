4b46853048bb3d392747f63ca0e4014737cdebc6
liuweichen789 <145267514+liuweichen789@users.noreply.github.com>
2025-09-21 10:55:58 +0800

    Add files via upload

---
+++ sierpinski.py
@@ -0,0 +1,62 @@
+"""
+File: sierpinski.py
+Name: wei chen
+---------------------------
+This file recursively prints the Sierpinski triangle on GWindow.
+The Sierpinski triangle is a fractal described in 1915 by Waclaw Sierpinski.
+It is a self similar structure that occurs at different levels of iterations.
+"""
+
+from campy.graphics.gwindow import GWindow
+from campy.graphics.gobjects import GLine
+from campy.gui.events.timer import pause
+
+# Constants
+ORDER = 2                  # Controls the order of Sierpinski Triangle
+LENGTH = 600               # The length of order 1 Sierpinski Triangle
+UPPER_LEFT_X = 150		   # The upper left x coordinate of order 1 Sierpinski Triangle
+UPPER_LEFT_Y = 100         # The upper left y coordinate of order 1 Sierpinski Triangle
+WINDOW_WIDTH = 950         # The width of the GWindow
+WINDOW_HEIGHT = 700        # The height of the GWindow
+
+# Global Variable
+window = GWindow(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)  # The canvas to draw Sierpinski Triangle
+
+
+def main():
+	"""
+	TODO:
+	階層1:畫一個長為LENGTH的正三角形
+	每增加一階層: 在三角形內部的左上，右上，下方，畫出長為LENGTH*0.5的三角形
+	三角形的右上頂點位置為: UPPER_LEFT_X, UPPER_LEFT_Y
+	"""
+	sierpinski_triangle(ORDER, LENGTH, UPPER_LEFT_X, UPPER_LEFT_Y)
+
+
+def sierpinski_triangle(order, length, upper_left_x, upper_left_y):
+	"""
+	:param order: int, 三角形有幾階層。
+	:param length: int, 三角形邊長。
+	:param upper_left_x: int, 三角形右上頂點的x座標
+	:param upper_left_y: int, 三角形右上頂點的y座標
+	:return: window.add(line)，畫出三角形
+	"""
+	if order == 0:
+		pass
+	else:
+		line1 = GLine(upper_left_x, upper_left_y, upper_left_x+length, upper_left_y)
+		line2 = GLine(upper_left_x, upper_left_y, upper_left_x+(length*0.5), upper_left_y+(length*0.866))
+		line3 = GLine(upper_left_x+length, upper_left_y, upper_left_x+(length*0.5), upper_left_y+(length*0.866))
+		window.add(line1)
+		window.add(line2)
+		window.add(line3)
+		# upper left
+		sierpinski_triangle(order-1, length*0.5, upper_left_x, upper_left_y)
+		# upper right
+		sierpinski_triangle(order - 1, length * 0.5, upper_left_x+(length*0.5), upper_left_y)
+		# lower
+		sierpinski_triangle(order - 1, length * 0.5, upper_left_x+(length*0.25), upper_left_y+(length*0.433))
+
+
+if __name__ == '__main__':
+	main()
